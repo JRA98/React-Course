bbd41b567687d9bfe61609056cc2cbba018a4001
Javi <javieruizanaya@gmail.com>
2020-08-31 15:59:28 +0200

    MERN avance

---
+++ MERNTasks/cliente/src/components/proyectos/NuevoProyecto.jsx
@@ -0,0 +1,80 @@
+import React, {Fragment, useState, useContext} from 'react'
+import proyectoContext from '../../context/proyectos/proyectoContext';
+
+const NuevoProyecto = () => {
+
+    // Obtener el state del formulario
+    const proyectosContext = useContext(proyectoContext);
+    const {formulario, mostrarFormulario} = proyectosContext;
+
+
+    // state parA pROYECTO
+
+    const [proyecto, guardarProyecto] = useState({
+        nombre : ''
+    });
+
+    const {nombre} = proyecto
+
+    //Lee los contenidos del input
+    const onChangeProyecto = e => {
+        guardarProyecto({
+            ...proyecto,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    // Cuando el usuario envia un proyecto
+    const onSubmitProyecto = e => {
+        e.prevent.Default();
+
+        //Validar el proyecto
+
+        //Agregar al state
+
+        //Reiniciar el form
+    }
+
+    //MOstrar el formulario
+    const onClickFormulario = () => {
+        mostrarFormulario()
+    }
+    return ( 
+        <Fragment>
+            <button
+                type="button"
+                className="btn btn-block btn-primario"
+                onClick={onClickFormulario}
+            >Nuevo proyecto</button>
+
+            {
+                formulario
+                ? 
+                    (
+                        <form
+                            className="formulario-nuevo-proyecto"
+                            onSubmit={onSubmitProyecto}
+                        >
+                            <input
+                                type="text"
+                                className="input-text"
+                                placeholder="Nombre Proyecto"
+                                name="nombre"
+                                value={nombre}
+                                onChange={onChangeProyecto}
+                            />
+
+                            <input 
+                                type="submit"
+                                className="btn btn-primario btn-block"
+                                value="Agregar Proyecto"
+                            />
+                        </form>
+                    )
+                : null
+            }
+        </Fragment>
+     );
+}
+ 
+export default NuevoProyecto;
