2df263bd9c6997d088ea23bdf069803f586779c1
Javi <javieruizanaya@gmail.com>
2020-11-09 19:51:27 +0100

    Init

---
+++ FirefoxSend/NodeSend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+
+// crear el servidor
+const app = express();
+
+
+//Conectar a la base de datos
+conectarDB();
+console.log('Iniciando el servidor');
+// Puerto de la app
+const port = process.env.PORT || 4000;
+
+//Habilitar leer los valores de un body
+app.use(express.json());
+
+
+//Rutas de la app
+app.use('/api/usuarios', require('./routes/usuarios'));
+
+
+//Arrancar la app
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor está funcionando en el puerto ${port}`);
+})
